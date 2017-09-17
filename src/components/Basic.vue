657acf64925b5d18e1ae43114f50b332f34f216e
Dino <jl_lsj@163.com>
2017-09-17 22:25:10 +0800

    add first components

---
+++ src/components/Basic.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="basic">
+    <h1>{{ msg }}</h1>
+    <h2>Data Binding</h2>
+    <div> <span>{{ oneWay }}</span></div>
+    <h2>Ecosystem</h2>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'basic',
+  data () {
+    return {
+      msg: 'The demo of vue directive',
+      oneWay: 'This is one way binding',
+      twoWay: 'This is tow way binding'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
